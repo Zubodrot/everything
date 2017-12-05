5f5e0ad2067eec2590346d5481683477f544be4d
Zubodrot <34109534+Zubodrot@users.noreply.github.com>
2017-12-06 00:21:49 +0300

    Update Student.java

--- src/ru/isbo1016/chumakov/prak6/pt1/Student.java
+++ src/ru/isbo1016/chumakov/prak6/pt1/Student.java
@@ -1,7 +1,5 @@
 package ru.isbo1016.chumakov.prak6.pt1;
 
-import java.util.Comparator;
-
 public class Student {
     public String name;
     public int IDnumber;
@@ -17,19 +15,6 @@ public class Student {
 
     public int getIDnumber() {
         return IDnumber;
-    }
-
-    public class CompareID implements Comparator<Student>{
-        public int compare(Student a, Student b){
-            if (a.getIDnumber()>b.getIDnumber()){
-                return 1;
-            }
-            else if (a.getIDnumber()<b.getIDnumber()){
-                return -1;
-            }
-            else return 0;
-        }
-
     }
 
 }
